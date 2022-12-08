0c94dbccd94bf73168db8c87e89fbef8d50078b1
UKhurshed <khurshedumarov777@gmail.com>
2022-12-08 12:15:50 +0500

    first commit

---
+++ gram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gram
+//
+//  Created by Khurshed Umarov on 08.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
