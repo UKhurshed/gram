8d553eed981d693eabd4b8a1603781c83562f698
UKhurshed <khurshedumarov777@gmail.com>
2022-12-09 16:49:11 +0500

    Implemented urlSessionFacade

---
+++ gram/Core/URLSessionFacade/URLSessionFacade.swift
@@ -0,0 +1,36 @@
+//
+//  URLSessionFacade.swift
+//  gram
+//
+//  Created by Khurshed Umarov on 09.12.2022.
+//
+
+import Foundation
+
+typealias ResultAPI = (Result<Data, CustomError>) -> Void
+
+protocol UrlSessionProtocol {
+    func registerUserByPhoneNumber<RegisterResponse: Decodable>(with apiRequest: APIRequest) async throws -> RegisterResponse
+}
+
+class URLSessionFacade: UrlSessionProtocol {
+    
+    private let urlSession: URLSession
+    
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+    
+    func registerUserByPhoneNumber<RegisterResponse: Decodable>(with apiRequest: APIRequest) async throws -> RegisterResponse {
+        let urlRequest = try apiRequest.getURLRequest()
+        let (data, response) = try await urlSession.data(for: urlRequest)
+        guard let httpURLResponse = response as? HTTPURLResponse else {
+            throw CustomError.nullData
+        }
+        print("registerUserByPhoneNumber statusCode: \(httpURLResponse.statusCode)")
+        
+        return try JSONDecoder().decode(RegisterResponse.self, from: data)
+    }
+    
+    
+}
