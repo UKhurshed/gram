8370ad14bf2d4615f45ea4e8dd98b706a661e12a
UKhurshed <khurshedumarov777@gmail.com>
2022-12-10 14:27:41 +0500

    Added a main screen and added logic when open main screen or register

---
+++ gram/Main/MainUIView.swift
@@ -0,0 +1,20 @@
+//
+//  MainUIView.swift
+//  gram
+//
+//  Created by Khurshed Umarov on 10.12.2022.
+//
+
+import UIKit
+
+class MainUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
